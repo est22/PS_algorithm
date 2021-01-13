a2afd03fc3dba314ee1e9e3a0c0111a50a757d4e
est22 <anlia.seoul@gmail.com>
2021-01-14 00:29:37 +0900

    곱하기 혹은 더하기

--- greedy/times_or_plus.py
+++ greedy/times_or_plus.py
@@ -1,10 +1,22 @@
-numbers = list(map(int,input().split()))
-total = 0
-while True:
-    for i in (0,len(numbers)):
-        if numbers[i] == 0 or numbers[i] == 1:
-            total += numbers[i] + numbers[i+1]
-            break
-        total += numbers[i] * numbers[i+1]
+numbers = input() #  02984
+result = int(numbers[0]) #  0
 
-print(numbers)
+for i in range (1,len(numbers)):
+    num = int(numbers[i]) 
+    if num <= 1 or result <= 1:
+        result += num
+    else: result *= num
+
+print(result)
+
+#  오답(초기)
+# numbers = list(map(int,input().split()))
+# total = 0
+# while True:
+#     for i in (0,len(numbers)):
+#         if numbers[i] == 0 or numbers[i] == 1:
+#             total += numbers[i] + numbers[i+1]
+#             break
+#         total += numbers[i] * numbers[i+1]
+
+# print(numbers)
