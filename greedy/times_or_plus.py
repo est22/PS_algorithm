a855d90e67c708adabcdcb52b0b67b8924cd865b
est22 <60957334+est22@users.noreply.github.com>
2021-01-14 00:31:31 +0900

    Update times_or_plus.py

--- greedy/times_or_plus.py
+++ greedy/times_or_plus.py
@@ -8,15 +8,3 @@ for i in range (1,len(numbers)):
     else: result *= num
 
 print(result)
-
-#  오답(초기)
-# numbers = list(map(int,input().split()))
-# total = 0
-# while True:
-#     for i in (0,len(numbers)):
-#         if numbers[i] == 0 or numbers[i] == 1:
-#             total += numbers[i] + numbers[i+1]
-#             break
-#         total += numbers[i] * numbers[i+1]
-
-# print(numbers)
