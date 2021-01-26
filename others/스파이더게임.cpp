ea2a8dd3615ac6f7c764717c5d4a3d21fc0d14f3
est22 <anlia.seoul@gmail.com>
2021-01-26 16:29:21 +0900

    1072

---
+++ others/스파이더게임.cpp
@@ -0,0 +1,32 @@
+#include <cstdio>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+#define MAX_N   1000
+#define long long ll;
+
+int A[MAX_N], B[MAX_N];
+vector<int> subA, subB;
+
+int main(){
+    ll X, Y, R, Z;
+
+    scanf("%lld %lld", &X, &Y);
+    R = Y * 100 / X;
+
+    if (R >= 99) printf("-1\n");
+    else {
+        ll low = 0, high = X, mid;
+        while (low <= high) {
+            mid = (low + high) >> 1; //2로 나눈 효과를 냄.
+            if ( (Y + mid) * 100 / (X + mid) > R) high =  mid - 1;
+            else low = mid + 1;
+        }
+        printf("&lld\n", high + 1);
+    }
+
+    return 0;
+
+}
