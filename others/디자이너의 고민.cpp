b557d7ac296902517a4369ef29f5646e38bcb950
est22 <anlia.seoul@gmail.com>
2021-02-10 23:36:56 +0900

    SDS Pro test

--- others/디자이너의 고민.cpp
+++ others/디자이너의 고민.cpp
@@ -1,1 +1,14 @@
 #include <iostream>
+#include <vector>
+#include <cstdio>
+
+#define MAX_N   50000
+
+using namespace std;
+
+int N, K;
+int mp1, mp2; // mandatory piece 1, 2
+
+int main(){
+    
+}
