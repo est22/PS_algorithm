dcd3782df2a0b695c09e7f8b5d404dd2e8689515
est22 <anlia.seoul@gmail.com>
2021-07-24 06:51:27 +0900

    210724 스택수열 * 문제 이해 완료

--- 스택,큐,덱/1874.py
+++ 스택,큐,덱/1874.py
@@ -1,28 +1,34 @@
-# 문제 이해가 잘..
-# 
+# 스택 수열
+# 처음엔 문제 이해가 잘.. 안갔지만 이해함.
+
 import sys
 input = sys.stdin.readline
 
-n = int(input())
-stack = []
-answer = []
-flag = 0
+n = int(input()) # 8
+stack = [] # 1, 2, 3, 4, ... (입력해야 할 숫자)
+answer = [] # + or -
+flag = True
 cur = 1
 
+
 for i in range(n):
-    num = int(input())
+    num = int(input()) # 4 3 6 8 7 5 2 1 (출력할 숫자)
+
     while cur <= num:
         stack.append(cur)
         answer.append('+')
         cur += 1
 
     if stack[-1] == num:
-        stack.pop
+        stack.pop()
         answer.append('-')
+
     else:
-        print("NO")
-        flag = 1
-        break
-if flag == 0:
+        flag = False
+
+
+if flag == True:
     for i in answer:
         print(i)
+else:
+    print('NO')
