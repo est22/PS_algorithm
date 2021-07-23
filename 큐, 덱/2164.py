44efc94f7084296260c14a44ae3f5ca4653eeb75
est22 <anlia.seoul@gmail.com>
2021-07-24 06:58:24 +0900

    210724 카드2 (덱 사용하는 문제)

--- 큐, 덱/2164.py
+++ 큐, 덱/2164.py
@@ -12,6 +12,7 @@ dq = deque(range(1, int(input()) + 1))
 
 while len(dq) != 1:
     dq.popleft() # 위쪽에 있는 카드 삭제
-    dq.popleft() # 위쪽에 있는 카드를 삭제해서 아래로 보낸다.
+    # 위쪽에 있는 카드를 삭제해서 아래로 보낸다.
+    dq.append(dq.popleft())
 
-print(dq)
+print(dq[0])
