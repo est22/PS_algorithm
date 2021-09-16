f20c9ba354b8ddcf2f5c220f942d4de4187510ca
est22 <anlia.seoul@gmail.com>
2021-09-16 10:00:35 +0900

    0916

---
+++ graph/2210.py
@@ -0,0 +1,28 @@
+import sys
+input = sys.stdin.readline
+
+# 그래프 입력받을 때 list comprehension
+matrix = [list(map(str, input().split())) for _ in range(5)]
+
+def dfs(x, y, number):
+    if len(number) == 6:
+        if number not in answer: #result에 없다면
+            answer.append(number)
+        return 
+        
+    dx = [1, -1, 0, 0]
+    dy = [0, 0, 1, -1] 
+
+    for i in range(4):
+        ddx = x + dx[i]
+        ddy = y + dy[i]
+        
+        if 0 <= ddx < 5 and 0 <= ddy < 5:
+            dfs(ddx, ddy, number + matrix[ddx][ddy]) 
+
+
+answer = []
+for i in range(5):
+    for j in range(5):
+        dfs(i, j, matrix[i][j])  # 0,0~4,4 검사
+print(len(answer))
